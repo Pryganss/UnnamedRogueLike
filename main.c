aaf8d7a996aad31cba00eb09a060e18a070131cf
Pryganss <thepullinger@gmail.com>
2022-06-10 15:11:46 +0100

    Added Movement

--- main.c
+++ main.c
@@ -1,41 +1,51 @@
-/*******
-* MAIN *
-********/
+//////////
+// MAIN //
+//////////
 
 #include <ncurses.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
 
 /* Structs */
-typedef struct Player
+typedef struct Entity
 {
-  int xPos;
   int yPos;
-  int hp;
+  int xPos;
+  char eCh;
   
-} Player;
+} Entity;
 
 /* Functions */
+bool tileWalkable(int y, int x);
 int screenSetUp();
 int mapSetUp();
-Player * playerSetup();
+int handleInput(int input, Entity * player);
+int entityMove();
+Entity * entitySetup(int y, int x, char ch);
+
 
 /* main */
 int main () {
 
   int ch;
-  Player * user;
+  Entity * player;
+
+  setlocale(LC_ALL, "");
   
   screenSetUp();
 
   mapSetUp();
   
-  user = playerSetup();
+  player = entitySetup(14, 14, '@');
 
-  while ((ch = getch()) != 'q')
-    {
-      
-    }
+  entityMove(player->yPos, player->xPos, player);
   
+  while ((ch = tolower(getch())) != 'q')
+    {
+      handleInput(ch, player); 
+    }
+   
   endwin();
   
   return 0;
@@ -46,6 +56,7 @@ int screenSetUp()
 {
   initscr();
   noecho();
+  curs_set(0); 
   refresh();
 
   return 0;
@@ -53,29 +64,79 @@ int screenSetUp()
 
 int mapSetUp()
 {
-  mvprintw(13, 13, " ------");
-  mvprintw(14, 13, "|......|");
-  mvprintw(15, 13, " ------") ;
+  mvprintw(13, 13, "┌─────────────┐");
+  mvprintw(15, 13, "│             │");
+  mvprintw(16, 13, "│             │");
+  mvprintw(17, 13, "│             │");
+  mvprintw(18, 13, "│             │");
+  mvprintw(19, 13, "│             │");
+  mvprintw(14, 13, "│             │");
+  mvprintw(20, 13, "└─────────────┘");
+
+  return 0;
 }
 
-Player * playerSetup()
+Entity * entitySetup(int y, int x, char ch)
 {
-  Player * newPlayer;
-  newPlayer = malloc(sizeof(Player));
+  Entity * newEntity;
+  newEntity = malloc(sizeof(Entity));
 
-  newPlayer->xPos = 14;
-  newPlayer->yPos = 14;
-  newPlayer->hp = 5;
-
-  mvprintw(newPlayer->yPos, newPlayer->xPos, "@");
+  newEntity->xPos = x;
+  newEntity->yPos = y;
+  newEntity->eCh = ch;
     
-  return newPlayer;
+  return newEntity;
 
 }
 
+int handleInput(int input, Entity * player)
+{
+  switch (input)
+    {
+      /* MOVE */
+    case 'a':
+      entityMove(player->yPos, player->xPos - 1, player);
+      break;
+      
+    case 'w':
+      entityMove(player->yPos - 1, player->xPos, player);
+      break;
+      
+    case 'd':
+      entityMove(player->yPos, player->xPos + 1, player);
+      break;
+      
+    case 's':
+      entityMove(player->yPos + 1, player->xPos, player);
+      break;
+      
+    default:
+      break;
+      
+    }
+  
+  return 0;
+}
 
+int entityMove(int y, int x, Entity * entity)
+{
+  if (tileWalkable(y, x)) 
+    {  mvprintw(entity->yPos, entity->xPos, " ");
+      
+      entity->yPos = y;
+      entity->xPos = x;
+      
+      mvprintw(entity->yPos, entity->xPos, "%c", entity->eCh);
 
+      return 0;
+    }
 
+  // Tile not walkable
+  return 1;
+}
 
-
+bool tileWalkable(int y, int x)
+{
+  return (mvinch(y, x) == ' ');
+}
 
